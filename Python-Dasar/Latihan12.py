b0a4358bd0acb64305cf01a826f9f59734c82e5b
Fadlan Hussein Al Utsman <husseinfadlan16@gmail.com>
2026-07-11 20:21:23 +0700

    Menambahkan Latihan 12 dan .gitignore

---
+++ Python-Dasar/Latihan12.py
@@ -0,0 +1,38 @@
+#%% Struktur task
+def function_name() :
+    #code
+    value = None
+    return value
+
+
+# %% Kasus 1
+def add(a, b):
+    return a + b
+
+
+result = add(10, 20)
+print(result)
+
+# %% kasus 2
+def rectangle_area (height, width):
+    return height*width
+
+result = rectangle_area(10, 20)
+print(result)
+
+# %% kasus 3
+def math_operations(a, b):
+    addition = a + b
+    subtraction = a - b
+    multiplication = a * b
+    divison = a / b
+    return addition, subtraction, multiplication, divison
+
+add, subtract, multiply, divide = math_operations (10, 2)
+print(result)
+
+# %% kasus 4
+
+
+
+
