397cd07087da8b604fc06d4a4124fac72dc38032
Fadlan Hussein Al Utsman <husseinfadlan16@gmail.com>
2026-07-22 16:53:40 +0700

    Finish latihan 22.py

--- Python-Dasar/D-22 Bank Account Simulator.py
+++ Python-Dasar/D-22 Bank Account Simulator.py
@@ -34,7 +34,7 @@ dog2.bark()
 class Person:
     def __init__(self, name, age):
         self.name = name
-        self.age = name
+        self.age = age
     
     def greet(self):
         print(f"Hello, my name is {self.name} and I am {self.age} years old")
@@ -44,3 +44,86 @@ person1.greet()
 
 # %% Kasus 4 Multiple Objects (Bank Simulator)
 
+class BankAccount:
+    def __init__(self, account_holder, initial_balance=0):
+        self.account_holder = account_holder
+        self.balance = initial_balance
+
+    #Deposit Money
+    def deposit(self, amount):
+        if amount > 0:
+            self.balance += amount
+            print(f"Deposited ${amount}, New Balance: ${self.balance}")
+        else:
+            print("Invalid deposit amount. Amount must be greater than 0.")
+
+    # Show Account Details
+    def show_details(self):
+        print(f"\n---- Account Details ----")
+        print(f"Account Holder: {self.account_holder}")
+        print(f"Account Balance: {self.balance}")
+
+# Main Program
+accounts = {}
+
+def create_account ():
+    name = input("Enter account holder's name: ").strip()
+    initial_deposit = float(input("Enter Initial Deposit Amount: "))
+    account = BankAccount(name, initial_deposit)
+    accounts[name] = account
+    print("Account created successfully")
+def withdraw(self, amount):
+    if amount > 0 and amount <= self.balance:
+        self.balance -= amount
+        print(f"Withdrawn ${amount}, New Balance: ${self.balance}")
+    else:
+        print(f"Invalid withdrawl amount or insufficient balance.")
+
+def access_account():
+    name = input("Enter your name: ").strip()
+    if name in accounts:
+        account = accounts[name]
+        while True:
+            print("\n---- Access Menu ----")
+            print("1. Deposit")
+            print("2. Withdraw")
+            print("3. Show Details")
+            print("4. Exit")
+            choice = input("Enter your choice(1-4): ")
+
+            if choice == '1':
+                amount = float(input("Enter deposit amount: "))
+                account.deposit(amount)
+            elif choice == '2':
+                amount = float(input("Enter withdraw amount: "))
+                account.withdraw(amount)
+            elif choice == '3':
+                account.show_details()
+            elif choice == '4':
+                print("Exiting account menu.")
+                break
+            else:
+                print("Invalid choice. Please select a valid option.")
+    else:
+        print("Account not found. Please create an account first.")
+
+# Main Menu
+while True:
+    print("\n---- Bank Account Simulator ----")
+    print("1. Create Account")
+    print("2. Access Account")
+    print("3. Exit")
+    choice = input("Enter your choice(1-3)")
+
+    if choice == '1':
+        create_account()
+    elif choice == '2':
+        access_account()
+    elif choice == '3':
+        print("Existing the program. Goodbye!")
+        break
+    else:
+        print("Invalid choice. Please select a valid option.")
+
+    
+# %%
