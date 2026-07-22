eceada3cd6fbfcbc5eb672f5bb08d47f1a25f84e
Fadlan Hussein Al Utsman <husseinfadlan16@gmail.com>
2026-07-22 17:40:32 +0700

    Latihan 23.py

--- Python-Dasar/D-23 Library Management System.py
+++ Python-Dasar/D-23 Library Management System.py
@@ -23,4 +23,41 @@ class BankAccount:
     def deposit(self, amount):
         self.balance += amount
         print(f"Deposited ${amount}, New Balance: ${self.balance}")
-        
+
+# Create an object
+
+account1 = BankAccount("John Doe", 1000)
+account1.deposit(500)
+
+# %% Kasus 3 Class Methods vs Static Method
+
+class Utility:
+    app_version = "1.0"
+
+    @classmethod
+    def get_version(cls):
+        print(f"App Version : {cls.app_version}")
+
+    @staticmethod
+    def greet():
+        print("Hello! Welcome to the app.")
+
+Utility.get_version()
+Utility.greet()
+
+# %% Kasus 4 Encapsulation and Validation
+
+class Account:
+    def __init__(self, owner, balance=0):
+        self.owner = owner
+        self.__balance = balance
+    
+    def deposit(self, amount):
+        if amount > 0:
+            self.__balance += amount
+            print(f"Deposited ${amount}. New Balance: ${self.__balance}")
+        else:
+            print("Invalid deposit amount")
+    
+    def get_balance(self):
+        return self
