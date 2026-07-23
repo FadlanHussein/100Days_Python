6b2454fcaff6969e9e265304e159ae3f686cd4b9
Fadlan Hussein Al Utsman <husseinfadlan16@gmail.com>
2026-07-23 10:01:40 +0700

    latihan type of inheritance

--- Python-Dasar/D-24 Employee Management System.py
+++ Python-Dasar/D-24 Employee Management System.py
@@ -31,3 +31,32 @@ class Child(Parent):
 child = Child()
 child.display()
 
+# %% Type of Inheritance
+class A:
+    def method_a(self):
+        print("I am method A")
+
+class B:
+    def method_b(self):
+        print("I am method B")
+
+class C(A, B):
+    pass
+obj = C()
+obj.method_a()
+obj.method_b()
+
+# %% Type of Inheritance
+
+class GrandParent:
+    def display(self):
+        print("I am Grand Parent class")
+
+class Parent(Grandparent):
+    pass
+class Child(Parent):
+    pass
+
+child = Child()
+child.display()
+
