2cdc384c4292292fde12500f53ba708f624a7682
Fadlan Hussein Al Utsman <husseinfadlan16@gmail.com>
2026-07-23 17:24:06 +0700

    Latihan project D-25

--- Python-Dasar/D-25 Animal Sound Simulator.py
+++ Python-Dasar/D-25 Animal Sound Simulator.py
@@ -57,3 +57,72 @@ duck = Duck()
 
 animal_sound(bird)
 animal_sound(duck)
+
+# %% Kasus 4 Real-World Examples of Polymorphism
+# Base Class
+class Animals:
+    def make_sound(self):
+        print("Some generic animal sound")
+
+# Devired Classes
+class Dog(Animal):
+    def make_sound(self):
+        print("Woof! Woof!")
+
+class Cat(Animal):
+    def make_sound(self):
+        print("Meow! Meow!")
+
+class Duck(Animal):
+    def make_sound(self):
+        print("Quack! Quack!")
+
+# Polymorphism
+class AnimalSoundSimulator:
+    def __init__(self):
+        self.animals = []
+
+    def add_animal(self, animal):
+        if isinstance(animals, Animal):
+            self.animals.append(animal)
+            print(f"{animal.__class__,__name__} added to the simulator")
+        else:
+            print("Invalid animal type") 
+
+    def make_all_sounds(self):
+        if not self.animals:
+            print("No animals in the simulator")
+        else:
+            print("\n---- Animal Sounds ----")
+            for animal in self.animals:
+                animal.make_sound()
+
+# Main Program
+simulator = AnimalSimulator()
+
+while True:
+    print("\n---- Animal Sound Simulator ----")
+    print("1. Add Dog")
+    print("2. Add Cat")
+    print("3. Add Cow")
+    print("4. Add Duck")
+    print("5. Make All Sound")
+    print("6. Exit")
+
+    choice = input("Enter your choice (1-6): ")
+
+    if choice == '1':
+        simulator.add.append_animal(Dog())
+    elif choice == '2':
+        simulator.add_animal(Cat())
+    elif choice == '3':
+        simulator.add.append_animal(Cow())
+    elif choice == '4':
+        simulator.add_animal(Cow())
+    elif choice == '5':
+        simulator.add_animal(Duck())
+    elif choice == '5':
+        print("Exiting the simulator. Goodbye!!!")
+        break
+    else:
+        print("Invalide choice. Please try again")
