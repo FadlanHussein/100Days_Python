35f43a29f4845898fe7a33e40c164e870d3ce014
Fadlan Hussein Al Utsman <husseinfadlan16@gmail.com>
2026-07-26 16:01:47 +0700

    Latihan D-29

---
+++ Python-Dasar/D-29 Simple GUI APP.py
@@ -0,0 +1,11 @@
+# Kasus 1 Tkinter (creating a basic tkinter window)
+
+import tkinter as tk
+
+# Create the main window
+root = tk.TK()
+root.title("My First GUI App")
+root.geometry("300x200")
+
+# Start the GUI event loop
+root.mainloop()
