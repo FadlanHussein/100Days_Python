30e7dff4519f74578da2da254e9b7f34dc56e0d6
Fadlan Hussein Al Utsman <husseinfadlan16@gmail.com>
2026-07-27 15:15:35 +0700

    latihan D-30.py kasus 4

--- Python-Dasar/D-30 Click Counter App.py
+++ Python-Dasar/D-30 Click Counter App.py
@@ -44,4 +44,55 @@ button.bind("<Leave>", on_leave)
 
 root.mainloop()
 
-# %%
+# %% Kasus 3: Dynamic Updates
+import tkinter as tk
+
+root = tk.Tk()
+root.title("Dynamic Center Button")
+root.geometry("300x200")
+
+# Counter Variable
+counter = 0
+
+def increment_counter():
+    global center
+    counter += 1
+    label.config(text=f"Count: {counter}")
+
+# Label and Button
+label = tk.Label(root, text="Count: 0", font=("Arial", 14))
+label.pack(pady=10)
+
+button = tk.Button(root, text="Click Me", command=increment_counter)
+button.pack(pady=10)
+
+root.mainloop()
+
+
+# %% Kasus 4: Button States
+
+import tkinter as tk
+
+# Main Window
+root = tk.Tk()
+root.title("Button Events")
+root.geometry("300x200")
+
+def toogle_button():
+    if button['state'] == 'normal':
+        button.config(state='disabled')
+        toggle_btn.config(text="Enable Button")
+    else:
+        button.config(state='normal')
+        toggle_btn.config(text="Disable Button")
+
+# Buttons
+button = tk.Button(root, text="I am Active")
+button.pack(pady=10)
+
+toggle_btn = tk.Button(root, text="Disable Button", command=toggle_button)
+toggle_btn.pack(pady=10)
+
+root.mainloop()
+
+
