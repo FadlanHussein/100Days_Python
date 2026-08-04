cce371e341b824f15ef7ff7635a9a9aa161b2084
Fadlan Hussein Al Utsman <husseinfadlan16@gmail.com>
2026-08-04 18:14:25 +0700

    Latihan D-38.py

---
+++ Python-Intermediete/D-38 Contact Form App/app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField, SubmitField
+from wtforms.validators import DataRequired, Email
+
+app = Flask(__name__)
+app.secret_key = 'your_secret_key'
+
+# Contact Form Class
+class ContactForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    message = TextAreaField('Message', validators=[DataRequired()])
+    submit = SubmitField('Send')
+
+@app.route('/', methods=['GET', 'POST'])
+def contact():
+    form = ContactForm()
+    if form.validate_on_submit():
+        flash(f'Message sent successfully {form.name.data}!', 'success')
+    return render_template('contact.html', form=form)
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5005)
