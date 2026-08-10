679889b10d4d5c6336e05358dc0b8436e79bf2dc
Fadlan Hussein Al Utsman <husseinfadlan16@gmail.com>
2026-08-10 17:02:16 +0700

    Latiha 2 dan 3 D-45.py

---
+++ Python-Intermediete/D-45 Graph Plotter Matplotlib/app2.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+
+categories = ["A", "B", "C", "D", "E"]
+values = [10, 20, 30, 40, 50]
+
+# Plot
+plt.bar(categories, values, color="skyblue")
+plt.title("Diagram Chart")
+plt.xlabel("Categories")
+plt.ylabel("Values")
+plt.show()
